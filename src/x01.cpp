eb4cb4263e4e2e00e65b8b90dc30dbb012b37b9d
Gustavo Carneiro <gcarneiroa@hotmail.com>
2020-08-07 16:57:35 -0300

    Remodeling

--- src/x01.cpp
+++ src/x01.cpp
@@ -1,73 +1,52 @@
 
 #include <algorithm>
-#include <ctime>
 #include <iostream>
+#include <QList>
 #include <QtAlgorithms>
+#include <chrono>
 
 const unsigned arraySize = 32768;
-
-inline void stlVersion(bool sorting)
+template<typename Vector> void test_branch_prediction(bool sorting, const char *type)
 {
-    clock_t start_time = clock();
-    {
-        int data[arraySize];
-        for (unsigned c = 0; c < arraySize; c++) {
-            data[c] = std::rand() % 256;
-        }
-        if (sorting) {
-            std::sort(data, data + arraySize);
-        }
-        long long sum = 0;
-        for (unsigned i = 0; i < 100000; i++) {
-            for (unsigned c = 0; c < arraySize; ++c) {
-                if (data[c] >= 128)
-                    sum += data[c];
+    
+    Vector array;
+    array.reserve(arraySize);
+
+    for (unsigned c = 0; c < arraySize; c++) {
+        array.push_back(std::rand() % 256);
+    }
+
+    std::chrono::steady_clock::time_point start = std::chrono::steady_clock::now();
+    if (sorting) {
+        std::sort(std::begin(array), std::end(array));
+    }
+
+    long long sum = 0;
+    for (unsigned i = 0; i < 100000; i++) {
+        for (unsigned c = 0; c < arraySize; ++c) {
+            if (array.at(c) >= 128) {
+                sum += array.at(c);
             }
         }
     }
-    double elapsed_time = static_cast<double>(clock() - start_time) / CLOCKS_PER_SEC;
-    if (sorting) {
-        std::cout << "STL Version, with sort, elapsed time: " << elapsed_time << std::endl;
-    } else {
-        std::cout << "STL Version, without sort, elapsed time: " << elapsed_time << std::endl;
-    }
-}
 
-inline void qtVersion(bool sorting)
-{
-    clock_t start_time = clock();
-    {
-        int data[arraySize];
-        for (unsigned c = 0; c < arraySize; c++) {
-            data[c] = std::rand() % 256;
-        }
-        if (sorting) {
-            //qSort(data.begin(), data.end());
-        }
-        long long sum = 0;
-        for (unsigned i = 0; i < 100000; i++) {
-            for (unsigned c = 0; c < arraySize; ++c) {
-                if (data[c] >= 128) {
-                    sum += data[c];
-                }
-            }
-        }
-    }
-    double elapsed_time = static_cast<double>(clock() - start_time) / CLOCKS_PER_SEC;
+    std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+    int result = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
+
     if (sorting) {
-        std::cout << "Qt Version, with sort, elapsed time: " << elapsed_time << std::endl;
+        std::cout << type << " with    sort, elapsed time: " << result << std::endl;
     } else {
-        std::cout << "Qt Version, without sort, elapsed time: " << elapsed_time << std::endl;
+        std::cout << type << " without sort, elapsed time: " << result << std::endl;
     }
 }
 
 int main(int argc, char **argv)
 {
-    stlVersion(true);
-    stlVersion(false);
-
-    qtVersion(true);
-    qtVersion(false);
+    test_branch_prediction<QVector<int>>(true,"QVector");
+    test_branch_prediction<QVector<int>>(false, "QVector");
+    
+    test_branch_prediction<std::vector<int>>(true, "std::vector");
+    test_branch_prediction<std::vector<int>>(false, "std::vector");
 
     return 0;
 }
